2b46a17a11a9b84300f2ced2804a4e6bc797ade3
Bigo <bigo@crisidev.org>
2021-01-12 18:17:13 +0000

    Add docker x86-64 build

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 6 ]; then
+  echo "usage: $0 <git repository> <repository name> <username> <peername>"
+  echo "example: $0 https://github.com/blackmesalab/novanet novanet bigo rpi-home"
+  exit 1
+fi
+
+REPOSITORY=$1
+REPONAME=$2
+USERNAME=$3
+PEERNAME=$4
+PRIVKEY=$5
+
+fireguard repo clone -r "${REPOSITORY}"
+fireguard wg -r "${REPONAME}" render -u "${USERNAME}" -p "${PEERNAME}" -P "${PRIVKEY}"
+fireguard wg -r "${REPONAME}" up
