d22036572bec1576ea68b6f4d91cdc42fa372840
Bigo <bigo@crisidev.org>
2021-01-30 17:45:10 +0000

    Spawn a tokio executor with multiple available workers to let the background task contacting github releases to work

--- src/bin/fireguard.rs
+++ src/bin/fireguard.rs
@@ -6,7 +6,10 @@ use color_eyre::eyre::Result;
 
 use fireguard::run;
 
-#[tokio::main]
+/// Since reqwest uses a tokio task underneath, I believe we need at
+/// least 2 available threads to ensure we can run both the upgrade task
+/// and the loop waiting for signals asyncronously
+#[tokio::main(worker_threads = 2)]
 async fn main() -> Result<()> {
     color_eyre::install()?;
     Ok(run().await?)
