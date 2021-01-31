ab5054845b23ed606e845231b01a3ad519e8b51e
Bigo <bigo@crisidev.org>
2021-01-31 14:48:13 +0000

    Try to overcome travis greed. Allow multiple tokio threads in current runtime

--- src/bin/fireguard.rs
+++ src/bin/fireguard.rs
@@ -9,7 +9,7 @@ use fireguard::run;
 /// Since reqwest uses a tokio task underneath, I believe we need at
 /// least 2 available threads to ensure we can run both the upgrade task
 /// and the loop waiting for signals asyncronously
-#[tokio::main(worker_threads = 2)]
+#[tokio::main(flavor="multi_thread")]
 async fn main() -> Result<()> {
     color_eyre::install()?;
     Ok(run().await?)
